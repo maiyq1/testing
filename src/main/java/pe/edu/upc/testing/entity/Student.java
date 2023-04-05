1cbafcfec6c1b9833fd72202fb9765e1c3a6f9b8
maiyq2002 <mijael.yq2002@gmail.com>
2023-04-05 13:39:38 -0500

    agregacion de dependencia allargsconstructor

--- src/main/java/pe/edu/upc/testing/entity/Student.java
+++ src/main/java/pe/edu/upc/testing/entity/Student.java
@@ -1,12 +1,14 @@
 package pe.edu.upc.testing.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class Student {
     private int id;
     private String lastName;
-    private String FirstName;
+    private String firstName;
 }
