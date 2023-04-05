203f3044b83f3bb5a611d1e62b8250e36585ec04
maiyq2002 <mijael.yq2002@gmail.com>
2023-04-05 13:30:26 -0500

    Entity - Student

---
+++ src/main/java/pe/edu/upc/testing/entity/Student.java
@@ -0,0 +1,12 @@
+package pe.edu.upc.testing.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Student {
+    private int id;
+    private String lastName;
+    private String FirstName;
+}
