05afd1afd84ebab64add967548393040b3a82f0a
maiyq2002 <mijael.yq2002@gmail.com>
2023-04-05 14:36:07 -0500

    UnitTest AAA StudentService
    Externo e interno

---
+++ src/test/java/pe/edu/upc/testing/service/StudentServiceTest.java
@@ -0,0 +1,37 @@
+package pe.edu.upc.testing.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.listeners.MockitoListener;
+import pe.edu.upc.testing.entity.Student;
+import pe.edu.upc.testing.repository.StudentRepositoryImpl;
+
+@ExtendWith(MockitoExtension.class)
+public class StudentServiceTest {
+    @InjectMocks
+    private StudentServiceImpl studentService;
+
+    @Mock
+    private StudentRepositoryImpl studentRepository;
+
+    @Test
+    public void testBuscarPorId(){
+        //Preparar: Tipo de dato esperado
+        Student espero = new Student(1, "Paterno", "Nombre");
+        //Mock: Simular
+        Mockito.when(studentRepository.findById(Mockito.anyInt()))
+                .thenReturn(new Student(1, "Paterno", "Nombre"));
+        //Act
+        Student recibo = studentRepository.findById(1);
+
+        //Afirmar
+        Assertions.assertEquals(espero.getId(), recibo.getId());
+        Assertions.assertEquals(espero.getLastName(), recibo.getLastName());
+        Assertions.assertEquals(espero.getFirstName(), recibo.getFirstName());
+    }
+}
